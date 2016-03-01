a8710407f090704af26fce3fb5926259ee5b416a
lance1313 <jlin3312@740s-pl-102.local>
2016-03-01 09:20:09 -0700

    added eve robot frame

---
+++ src/robot/model/EV3Bot.java
@@ -0,0 +1,46 @@
+package robot.model;
+import lejos.hardware.ev3.LocalEV;
+//import lejos.hardware
+//import lejos.hardware
+//import lejos.hardware
+//import lejos.robotics
+//import lejos.robotics
+//import lejos.robotics
+//import lejos.utility
+import lejos.hardware.*;
+//import lejos.hardware
+
+public class EV3Bot 
+{
+	
+	private void setupPilot()
+	{
+		
+	}
+	
+	public void driveRoom()
+	{
+		
+	}
+	
+	public void displayMessage()
+	{
+		
+	}
+	
+	public void displayMessage(String message)
+	{
+		
+	}
+	
+	public void rotate()
+	{
+		
+	}
+	
+	public void travel()
+	{
+		
+	}
+	
+}
