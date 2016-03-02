fd42bc8a41d4125e243bb4512caab62bbe62eae0
lance1313 <jlin3312@740s-pl-102.local>
2016-03-02 13:23:54 -0700

    added robot ledges library files

--- src/robot/model/EV3Bot.java
+++ src/robot/model/EV3Bot.java
@@ -15,17 +15,34 @@ public class EV3Bot
 	
 	private void setupPilot()
 	{
-		
+		Wheel leftWheel = WeeledChassis.modelWheel(Motor.A, 55.0).offset(-72);
+		Wheel rightWheel =  WheeledChassis.moderlWheel(Motor.B,55.0).offset(-72);
+		WheeledChassis chassis = new WheeledChassis(new Wheel[] { leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+		botPilot = new MovePilot(chassis);
 	}
 	
 	public void driveRoom()
 	{
+		ultrasonicSamples = new float [distanceSensor.sampleSize()];
+		distanceSensor.fetchSample(ultrasonicSamples, 0);
+		if(ultrasonicSample[0] < 2.5)
+		{
+			displayMessage("I amat the door!");
+			driveShort();
+		}
 		
+		else
+		{
+			displayMessage("Long drive");
+			driveLong();
+		}
+		displayMessage("I am at the door!");
 	}
 	
 	public void displayMessage()
 	{
-		
+		LCD.drawString(botMessage, xPosition,yPosition);
+		Delay.mselay(waitTime);
 	}
 	
 	public void displayMessage(String message)
